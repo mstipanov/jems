d9df18a1f3c6debc6b8d14e6aada8cd6af8d6dd6
Marko Stipanov <marko.stipanov@gmail.com>
2010-11-27 20:34:51 +0100

    init.ver.

---
+++ src/main/java/hr/sting/jems/web/controller/AdminController.java
@@ -0,0 +1,13 @@
+package hr.sting.jems.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author mstipanov
+ * @since 17.10.10. 18:46
+ */
+@Controller
+@RequestMapping(value = "/admin")
+public class AdminController {
+}
